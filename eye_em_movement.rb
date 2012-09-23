1249b49c9f0b20019cbd5836fe93b038c39e55b3
André Rieck <4ndr3r13ck@googlemail.com>
2012-09-23 04:10:26 +0200

    show photos on the map and more

--- eye_em_movement.rb
+++ eye_em_movement.rb
@@ -14,42 +14,57 @@ get '/' do
 end
 
 get '/load_movement' do #/:lat/:lng/:start_date/:end_date' do
-  lat ||= 52.5299
-  lng ||= 13.4115
-  start_date = DateTime.parse(start_date) rescue DateTime.new(2012,9,22,11,0,0,'+2')
-  end_date = DateTime.parse(end_date) rescue DateTime.now
+  #lat ||= 52.5299
+  #lng ||= 13.4115
+  lat = 52.52515030
+  lng = 13.36928844
+  start_date = DateTime.parse(start_date) rescue DateTime.new(2012,8,24,20,0,0,'+2')
+  end_date = DateTime.parse(end_date) rescue start_date+5.hours #DateTime.now
   datetime_steps = start_date.step(end_date,10.minutes).to_a
-  buckets = datetime_steps.inject([]) {|t,z| t << z.to_s; t << []}
+  buckets = datetime_steps.inject([]) {|t,z| t << z.to_i; t << []}
   buckets = Hash[*buckets]
-  logger.info buckets
-  nearby_venue_ids = EyeEmConnector.albums(:geoSearch => 'nearbyVenues', :lat => lat, :lng => lng)['albums']['items'].map{|w|w['id']}
+  points = [[lat,lng],[lat+0.0045,lng],[lat,lng-0.0045],[lat-0.0045,lng],[lat,lng+0.0045]]
+  nearby_venues = []
+  points.each do |point|
+    nearby_venues << EyeEmConnector.albums(:geoSearch => 'nearbyVenues', :lat => point[0], :lng => point[1])['albums']['items']
+  end
+  nearby_venue_ids = nearby_venues.flatten.map{|w|w['id']}.uniq
+  logger.info nearby_venue_ids.count
   nearby_venue_ids.each do |venue_id|
     offset = 0
     earliest_photo_date = DateTime.now
     tries = 5
-    while tries > 0 && earliest_photo_date > (start_date-5.minutes) do
+    while tries > 0 && earliest_photo_date > (start_date-30.minutes) do
       ps = EyeEmConnector.album_photos(venue_id,{:limit => 50, :offset => offset, :detailed => true})['photos']['items']
       ps.each do |photo|
         photo_datetime = DateTime.parse(photo['updated'])
-        buckets[start_date.to_s] << photo if photo_datetime > start_date-5.minutes && photo_datetime < start_date+5.minutes 
+        buckets[start_date.to_i] << photo if photo_datetime > start_date-30.minutes && photo_datetime < start_date+30.minutes #&& photo['latitude'].to_f > lat-0.008 && photo['latitude'].to_f < lat+0.008 && photo['longitude'].to_f > lng-0.008 && photo['longitude'].to_f < lng+0.008
       end
       offset += 50
       tries -= 1
       earliest_photo_date = DateTime.parse(ps.last['updated']) if ps.last.present?
     end
   end
-  user_ids = buckets[start_date.to_s].map {|photo| photo['user']['id']}.uniq
+  user_ids = buckets[start_date.to_i].map {|photo| photo['user']['id']}.uniq
+  logger.info user_ids
   user_ids.each do |user_id|
     offset = 0
-    ps = EyeEmConnector.user_photos(user_id,{:limit => 50, :offset => offset, :detailed => true})['photos']['items']
-    ps.each do |photo|
-      photo_datetime = DateTime.parse(photo['updated'])
-      datetime_step = round_datetime(photo_datetime,datetime_steps)
-      logger.info datetime_step
-      buckets[datetime_step.to_s] << photo if datetime_step.present?
-    end
+    tries = 5
+    earliest_photo_date = DateTime.now
+    #while tries > 0 && earliest_photo_date > (start_date-5.minutes) do
+      ps = EyeEmConnector.user_photos(user_id,{:limit => 50, :offset => offset, :detailed => true})['photos']['items']
+      ps.each do |photo|
+        photo_datetime = DateTime.parse(photo['updated'])
+        datetime_step = round_datetime(photo_datetime,datetime_steps)
+        buckets[datetime_step.to_i] << photo if datetime_step.present? #&& photo['latitude'].to_f > lat-0.008 && photo['latitude'].to_f < lat+0.008 && photo['longitude'].to_f > lng-0.008 && photo['longitude'].to_f < lng+0.008
+      end
+      offset += 50
+      tries -= 1
+      earliest_photo_date = DateTime.parse(ps.last['updated']) if ps.last.present?
+    #end
   end
   buckets.to_json
+
   #nearby_venue_ids.to_json
 end
 
--- public/application.js
+++ public/application.js
@@ -1,12 +1,164 @@
+var movement, map, markersContainer;
 $(function() {
   nokia.Settings.set("appId", "pGWrn1YOkrGt4uaqlj0e"); 
   nokia.Settings.set("authenticationToken", "YKMKiMya8wZ0QY3V7NRy2A");
 
-  var map = new nokia.maps.map.Display(
+  map = new nokia.maps.map.Display(
     document.getElementById("mapContainer"), {
+      components: [ 
+            // Behavior collection
+            new nokia.maps.map.component.Behavior(),
+            new nokia.maps.map.component.ZoomBar(),
+            //new nokia.maps.map.component.Overview(),
+            //new nokia.maps.map.component.TypeSelector(),
+            new nokia.maps.map.component.ScaleBar() ],
         // Zoom level for the map
         'zoomLevel': 10,
         // Map center coordinates
         'center': [52.51, 13.4] 
     });
+  markersContainer = new nokia.maps.map.Container();
+  map.objects.add(markersContainer);
+  // a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
+  $( "#dialog:ui-dialog" ).dialog( "destroy" );
+  // insert datepicker
+
+  $( "#date" ).datepicker();
+  
+  
+  var place = $( "#place" ),
+    userdate = $( "#date" ),
+    allFields = $( [] ).add( place ).add( date ),
+    tips = $( ".validateTips" );
+
+  function updateTips( t ) {
+    tips
+      .text( t )
+      .addClass( "ui-state-highlight" );
+    setTimeout(function() {
+      tips.removeClass( "ui-state-highlight", 1500 );
+    }, 500 );
+  }
+
+  function checkLength( o, n, min, max ) {
+    if ( o.val().length > max || o.val().length < min ) {
+      o.addClass( "ui-state-error" );
+      updateTips( "Length of " + n + " must be between " +
+        min + " and " + max + "." );
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function checkRegexp( o, regexp, n ) {
+    if ( !( regexp.test( o.val() ) ) ) {
+      o.addClass( "ui-state-error" );
+      updateTips( n );
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
+  $( "#dialog-form" ).dialog({
+    autoOpen: true,
+    height: 400,
+    width: 350,
+    modal: true,
+    buttons: {
+      "start search": function() {
+        var bValid = true;
+        allFields.removeClass( "ui-state-error" );
+        
+        var datum = Date.parse($('#date').val());
+        //console.log(datum);
+        //timestamp ausgeben
+        //$('<p>Start '+datum+'</p>').insertAfter('#zeit');
+        //Zeitspanne Beginn
+        var enddatum = 1345831200 + (5*60*60);
+        //$('<p>Ende '+enddatum+'</p>').insertAfter('#zeit');
+        //console.log(enddatum-datum);
+        // set slider values after init of slider
+        //$( "#slider" ).slider( "option", "min", datum, "max", enddatum );
+        // slider 0 till unix timestamp difference of 3 days in 10 minutes steps
+        $( "#slider" ).slider( "option", "min", 1345831200);
+        $( "#slider" ).slider( "option", "max", enddatum);
+        $( "#slider" ).slider( "option", "step", 600 );
+        //place for search query posting down here
+        $('#slider').slider("value", 1345831200)
+        $.getJSON('/load_movement', function(data) {
+          movement = data;
+          drawHeatMap(map,movement,$('#slider').slider("value"));
+        });
+        
+        $( this ).dialog( "close" );
+      },
+      Cancel: function() {
+        $( this ).dialog( "close" );
+      }
+    },
+    close: function() {
+      allFields.val( "" ).removeClass( "ui-state-error" );
+    }
+  });
+    
+  $( "#slider" ).slider({
+    change: function(event, ui)
+     { var nextunixtime = $('#slider').slider("value");
+      if (movement) drawHeatMap(map,movement,nextunixtime);
+      //console.log(nextunixtime);
+     }
+  });  
+  
+   //$( ".selector" ).slider({ min: -7 });
 })
+
+function drawHeatMap (map,movement,timestamp) {
+  var heatmapProvider;
+  map.overlays.clear();
+  markersContainer.objects.clear();
+  var heatMapDataSet = locationsToHeatMapData(movement[timestamp]);
+  colorGradient = {
+    stops: {
+      "0": "#0066FF",
+      "0.25": "#2657D9",
+      "0.5": "#73388C",
+      "0.75": "#A62459",
+      "1": "#FF0000"},
+    interpolate: false};
+
+  heatmapProvider = new nokia.maps.heatmap.Overlay({
+    max: 20,
+    colors: colorGradient,
+    opacity: 0.5,
+    coarseness: 1,
+    type: "density"
+  });
+  
+  // Read data into our Heatmap
+  heatmapProvider.addData(heatMapDataSet);
+  // Add it to the map's object collection so it will be rendered onto the map
+  map.overlays.add(heatmapProvider);
+  for (var i = 0; i < movement[timestamp].length; i++) {
+    var coord = new nokia.maps.geo.Coordinate(parseFloat(movement[timestamp][i]['latitude']),parseFloat(movement[timestamp][i]['longitude']))
+    markersContainer.objects.add(new nokia.maps.map.StandardMarker(coord))
+    $('#photoContainer').append('<img src="'+movement[timestamp][i]['thumbUrl']+'">')
+  }
+}
+
+var locationsToHeatMapData = function (locations) {
+  var heatmapData = [],
+    location,
+    i, 
+    len = locations.length;
+  for (i = 0; i < len; i++) {
+    location = locations[i];
+    heatmapData.push({
+        longitude: location.longitude,
+        latitude: location.latitude
+      }
+    );
+  }
+  return heatmapData;
+};
